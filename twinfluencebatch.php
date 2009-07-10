0defec9178e600f249b661cc9136641da1469e68
mchelen <mike@michaelchelen.net>
2009-07-10 13:10:56 -0400

    delay option now applies to initial try errors returned from api call no longer cause termination

--- twinfluencebatch.php
+++ twinfluencebatch.php
@@ -1,56 +1,83 @@
 <?php
 /* Script to return an XML file from Twinfluence site
  * given user list as comma, tab, or line return seperated
- * 
- * 2009-07-06 Written: Michael Chelen http://mikechelen.com 
+ *
+ * 2009-07-06 Written: Michael Chelen http://mikechelen.com
  * License: Creative Commons Public Domain CC0
  * http://creativecommons.org/publicdomain/zero/1.0/
- * 
+ *
  */
-
-
-// initial delay (after failure)
-// subsequent retries will increase * 10 until max delay is surpassed
-// in 1/1000000s of a second
-$delay = 1000; // initial retry will be delayed 1/1000 of a second
-$maxDelay = 1000000; // maximum retry delay before giving up
-
 
 // accepts input either through form GET or command line arguments
 if ($argc > 1 || isset($_GET["user"])) {
+    // use command line arguments if they exist
     if ($argc > 1) {
         // echo "first " . $argv[0];
         $user = $argv[1];
         $pwd = $argv[2];
         $cacheonly = $argv[3];
         $idListUrl = $argv[4];
+        // initial delay
+        // subsequent retries will increase * 10 until max delay is surpassed
+        // in 1/1000000s of a second
+
+        if(isset($argv[5])){
+            $delay = $argv[5];
+        }
+        else {
+            $delay = 1000; // initial retry will be delayed 1/1000 of a second
+        }
+
+        if(isset($argv[6])){
+            $maxDelay = $argv[6];
+        }
+        else {
+            $maxDelay = 1000000; // maximum retry delay before giving up
+        }
+
     }
+    // use form input if it exists
     elseif (isset($_GET["user"]) && isset($_GET["pwd"]) && isset($_GET["idlist"]) && isset($_GET["cacheonly"])) {
         header('Content-Type: text/xml');
         $user = $_GET["user"];
         $pwd = $_GET["pwd"];
         $cacheonly = $_GET["cacheonly"];
         $idListUrl = $_GET["idlist"];
+
+        if(isset($_GET["delay"])){
+            $delay = $_GET["delay"];
+        }
+        else {
+            $delay = 1000; // initial retry will be delayed 1/1000 of a second
+        }
+
+        if(isset($_GET["maxdelay"])){
+            $maxDelay = $_GET["maxdelay"];
+        }
+        else {
+            $maxDelay = 1000000; // maximum retry delay before giving up
+        }
+
+
     }
     $idlist = file_get_contents($idListUrl);
-    
+
     // split the input at line returns, tabs, and commas
     $idlist = preg_split('/[\r\n\t,]+/', $idlist, -1, PREG_SPLIT_NO_EMPTY);
-    
+
     for ($i=0;$i<count($idlist);$i++) {
         // build the query with current username
         $id = $idlist[$i];
         $data = array ('user' => $user, 'pwd' => $pwd, 'id' => $id, 'cacheonly' => $cacheonly);
         $data = http_build_query($data);
         $url = 'http://twinfluence.com/api_user.php';
-        
+
         // send POST request and return the response
         $returned1 = do_post_request($url, $data, $delay, $maxDelay);
-        
+
         // replace "&" symbol with html entity
         $returned = str_replace("&","%26",$returned1);
-        
-        
+
         // cut off the first line
         $result .= substr($returned, stripos($returned,"\n"));
     }
@@ -69,6 +96,8 @@ else {
     Password: <input type="text" name="pwd" /><br />
     Cacheonly: <input type="text" name="cacheonly" value="TRUE"/><br />
     Username List URL (CSV): <input type="text" name="idlist"><br />
+    Retry Delay (millionths of a second): <input type="text" name="delay" value="1000"/><br />
+    Max Retry Delay (millionths of a second): <input type="text" name="maxdelay" value="1000000"/><br />
     <input type="submit" value="Submit" />
     </form>
     </body>
@@ -77,11 +106,12 @@ else {
 }
 
 
-// function to send a form POST
-// retries with increasing delay
-// original by Wez Furlong http://netevil.org/blog/2006/nov/http-post-from-php-without-curl
+// sends a POST
+// retry with increasing delay
+// original function by Wez Furlong http://netevil.org/blog/2006/nov/http-post-from-php-without-curl
 function do_post_request($url, $data, $delay, $maxDelay, $optional_headers = null)
 {
+    usleep($delay);
     $params = array('http' => array(
                   'method' => 'POST',
                   'content' => $data
@@ -93,21 +123,19 @@ function do_post_request($url, $data, $delay, $maxDelay, $optional_headers = nul
     $fp = @fopen($url, 'rb', false, $ctx);
     if (!$fp) {
         if ($delay >= $maxDelay ) {
-            throw new Exception("Problem with $url, $data, $php_errormsg");
+            return "Problem with $url, $data, $php_errormsg";
         }
         else {
-           usleep($delay);
-           return do_post_request($url, $data, $delay*10, $maxDelay);
+            return do_post_request($url, $data, $delay*10, $maxDelay);
         }
     }
     $response = @stream_get_contents($fp);
     if ($response === false) {
-    if ($delay >= $maxDelay) {
-            throw new Exception("Problem reading data from $url, $data, $php_errormsg");
+        if ($delay >= $maxDelay) {
+            return "Problem reading data from $url, $data, $php_errormsg";
         }
         else {
-           usleep($delay);
-           return do_post_request($url, $data, $delay*10, $maxDelay);
+            return do_post_request($url, $data, $delay*10, $maxDelay);
         }
     }
     return $response;
